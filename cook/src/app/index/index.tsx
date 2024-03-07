215f175041389770f8224ffb717151c0a9ec8071
GabrielVilhenaMagri <gabriel.vilhenamagri@gmail.com>
2024-03-07 17:10:15 -0300

    subindo o projeto

---
+++ cook/src/app/index/index.tsx
@@ -0,0 +1,69 @@
+
+import {View, Text, ScrollView, Alert } from 'react-native';
+import { router } from 'expo-router';
+import { styles } from './styles';
+import {Selected} from '../../components/Selected';
+import {useState} from 'react';
+import { Ingredient } from "@/components/Ingredient"
+import { useEffect } from 'react';
+import {services} from "@/services"
+
+
+export default function Index() {
+
+
+  const [selected, setSelected] = useState<String[]>([])
+  const [ingredients, setIngredients] = useState<IngredientResponse[]>([])
+
+  function handleToggleSelected(value: string ) {
+    if(selected.includes(value)) {
+      return setSelected((state) => state.filter((item) => item !== value))
+    }
+    setSelected((state) => [...state, value])
+  }
+
+  function handleClearSelected() {
+    Alert.alert("Limpar", "Deseja limpar tudo?", [
+      {text: "Não", style: 'cancel'},
+      {text: "Sim", onPress: () => setSelected([])}
+      
+    ])
+    
+  }
+
+  function handleSearch() {
+    router.navigate("/recipes/" + selected)
+  }
+
+  useEffect(() =>{
+    services.ingredients.findAll().then(setIngredients)
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Escolha {"\n"}
+      <Text style={styles.subtitle}>os produtos</Text>
+      </Text>
+
+      <Text style={styles.message}>
+        Descubra receitas baseadas nos
+      produtos que escolheu.
+      </Text>
+
+      <ScrollView  contentContainerStyle={styles.ingredients} showsVerticalScrollIndicator={false}>
+      { Array.from(ingredients).map((item) => (
+      <Ingredient key={item.id}
+      name={item.name}
+      image={`${services.storage.imagePath}/${item.image}`}
+      selected={selected.includes(item.id)} 
+      onPress={() => {handleToggleSelected(item.id)}}/>
+      ))}  
+    </ScrollView>
+      {
+        selected.length > 0  && (
+     <Selected quantity={selected.length} onClear={handleClearSelected} onSearch={handleSearch}/>
+      )}
+
+    </View>
+  )
+}
